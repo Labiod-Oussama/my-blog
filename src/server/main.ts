5a1a36820c66c3017ba7b78d11e92620a3c05521
Oussama_LD <oussama.labiod@inttic.dz>
2023-08-25 21:21:47 +0100

    deploy3

--- src/server/main.ts
+++ src/server/main.ts
@@ -3,12 +3,12 @@ import ViteExpress from "vite-express";
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
-const path=require('path');
+const path = require('path');
 const app = express();
 require('dotenv').config()
 const RegisterRoutes = require('./Routes/register');
-const BlogRoutes=require('./Routes/blogs');
-const ProfileRoutes=require('./Routes/profile')
+const BlogRoutes = require('./Routes/blogs');
+const ProfileRoutes = require('./Routes/profile')
 //cors
 const cors = require('cors');
 const corsOpts = {
@@ -22,15 +22,15 @@ const corsOpts = {
     'Content-Type', 'Authorization'
   ],
 };
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(cors(corsOpts))
 app.use(express.json())
 app.use(cookieParser());
 app.use(morgan('dev'));
 //db connection 
-const dburl = 'mongodb://localhost:27017/blogs';
-mongoose.connect(dburl, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => ViteExpress.listen(app, 3000, () => {
+// const dburl = 'mongodb://localhost:27017/blogs';
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => ViteExpress.listen(app, 3001, () => {
     console.log("Server is listening on port 3000...");
   }))
   .catch((err: any) => console.log(err))

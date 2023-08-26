807d007652847e6f47b1139d73f5a643a3fa5262
Oussama_LD <oussama.labiod@inttic.dz>
2023-08-26 20:40:58 +0100

    'depl'

--- src/server/main.ts
+++ src/server/main.ts
@@ -29,12 +29,12 @@ app.use(cookieParser());
 app.use(morgan('dev'));
 //db connection 
 // const dburl = 'mongodb://localhost:27017/blogs';
+// mongoose.connect(process.env.MONGO_URI)
 mongoose.connect(process.env.MONGO_URI)
   .then(() => app.listen(process.env.PORT, () => {
     console.log("Server is listening on port 3001...");
   }))
   .catch((err: any) => console.log(err))
-console.log('ok');
 
 app.use(RegisterRoutes);
 app.use(BlogRoutes);

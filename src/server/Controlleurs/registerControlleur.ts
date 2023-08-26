62b93a61f864a6fd3e535a9b4f338a2bea2a1996
Oussama_LD <oussama.labiod@inttic.dz>
2023-08-27 00:52:32 +0100

    'depp'

--- src/server/Controlleurs/registerControlleur.ts
+++ src/server/Controlleurs/registerControlleur.ts
@@ -10,7 +10,7 @@ module.exports.signup = async (req: Request, res: Response) => {
         const user = await User.create({ FirstName, LastName, Email, Password })
         const token = await user.generateAuthToken();
         res.cookie('token', token, {
-            maxAge: 1000 * 60 * 60 * 24
+            maxAge: 1000 * 60 * 60 * 24, httpOnly: true, secure: true, sameSite: 'strict', path: '/', domain: '.netlify.app'
         });
         res.status(201).json({ success: true, user, token })
     } catch (error) {
@@ -24,7 +24,7 @@ module.exports.login = async (req: Request, res: Response) => {
         const user = (req as any).user;
         const token = await user.generateAuthToken();
         res.cookie('token', token, {
-            maxAge: 1000 * 60 * 60 * 24, httpOnly: true, secure: true, sameSite: 'strict'
+            maxAge: 1000 * 60 * 60 * 24, httpOnly: true, secure: true, sameSite: 'strict', path: '/', domain: '.netlify.app'
         });
         res.status(201).json({ success: true, user, token })
     }
